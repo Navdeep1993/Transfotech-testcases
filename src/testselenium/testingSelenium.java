d5c0e2191e08ecdef9cd4809219ffd83ebc893a8
Navdeep <Navdeep@DESKTOP-D91PMTR.fios-router.home>
2022-02-11 17:10:30 -0500

    Testing Selenium - Navdeep

---
+++ src/testselenium/testingSelenium.java
@@ -0,0 +1,15 @@
+package testselenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testingSelenium {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "E:\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("");
+
+	}
+
+}
